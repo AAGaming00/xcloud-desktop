e6d67a5c1504bf9af8177c4789d04eaf85b91a14
AAGaming00 <aa@bigdumb.gq>
2021-04-22 17:17:46 -0400

    maybe possibly auto update??????????????

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const { app, BrowserWindow, ipcMain } = require('electron');
 const path = require('path');
+require('update-electron-app')()
 
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
 if (require('electron-squirrel-startup')) { // eslint-disable-line global-require
